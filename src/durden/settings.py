bc2dbafc456df01c0b5d5d46a8628c37cdc6d1dc
Mallem Sofiane <melsopv@gmail.com>
2026-07-08 03:33:47 +0200

    fixing env issue for ci, passed env required variables, no secrets included just general info, updated settings class to pydanticv2 standards

--- src/durden/settings.py
+++ src/durden/settings.py
@@ -1,4 +1,4 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 from pathlib import Path
 
 
@@ -9,8 +9,7 @@ class Settings(BaseSettings):
     version: str
     static_dir: Path
 
-    class Config:
-        env_file = ".env"
+    model_config = SettingsConfigDict(env_file=".env")
 
 
 settings = Settings()
